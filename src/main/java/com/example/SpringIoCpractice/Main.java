6c3171aa28b2e5ac2abc09cbbf082f150db3d564
SKB <1215237417@qq.com>
2019-07-29 12:40:24 +0800

    修改“ git commit -m xiugai git commit -m 修改”

--- src/main/java/com/example/SpringIoCpractice/Main.java
+++ src/main/java/com/example/SpringIoCpractice/Main.java
@@ -1,1 +1,27 @@
-//TODO
+
+package com.example.SpringIoCpractice;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.context.ApplicationContext;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+@SpringBootApplication
+
+public class Main {
+
+  public static void main(String[] args) {
+
+        SpringApplication.run(HelloWorld.class, args);
+
+        ApplicationContext ctx = new FileSystemXmlApplicationContext("hello.xml");
+
+        HelloWorld hw = (HelloWorld) ctx.getBean("helloworld");
+
+        hw.getHello();
+ }
--- src/test/java/com/example/SpringIoCpractice/SpringIoCPracticeApplicationTests.java
+++ src/test/java/com/example/SpringIoCpractice/SpringIoCPracticeApplicationTests.java
@@ -1,16 +1,19 @@
 package com.example.SpringIoCpractice;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringIoCPracticeApplicationTests {
-
 	@Test
 	public void contextLoads() {
+		ApplicationContext ctxx = new FileSystemXmlApplicationContext("hello.xml");
+		// 2.从容器中获取Bean实例
+		HelloWorld ho = (HelloWorld) ctxx.getBean("helloworld");
+		ho.getHello();
 	}
-
 }
